84d25b70c0316eb6768bc4060a8284381b29a5c3
EzéchielDev <yaoezchiel@gmail.com>
2026-03-13 09:22:15 +0000

    modification dans le dashboard

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { DashboardController } from './dashboard.controller';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [DashboardService],
-  controllers: [DashboardController]
+  controllers: [DashboardController],
 })
 export class DashboardModule {}
